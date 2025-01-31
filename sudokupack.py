20ff62f499728f3b88e98dee9b8eaf824a3d1d99
Pierre Maurier <pierre.maurier@gadz.org>
2025-01-31 15:55:51 +0100

    First working version

--- sudokupack.py
+++ sudokupack.py
@@ -5,37 +5,66 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('input', type=argparse.FileType('r'), help='input filename')
 parser.add_argument('output', nargs='?', help='pdf output filename - default to csv name')
+parser.add_argument('--ppp', type=int, default=2, help="Nbr of puzzle per page")
 
 args = parser.parse_args()
 
-# url_check = re.compile(r'^(http|https):\/\/([\w.-]+)(\.[\w.-]+)+([\/\w\.-]*)*\/?$')
 url_check = re.compile(r'(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})(\.[a-zA-Z0-9]{2,})?')
 
-
 import fpdf
+from fpdf.enums import XPos, YPos
 from PIL import Image
 
 pdf = fpdf.FPDF(orientation='P', format='A4', unit='mm')
 pdf.add_page()
 
-pdf.set_font("Helvetica", "B", 14)
-margins = 20
-puzzle_margin = 50
+pdf.add_font('Roboto', '', "Roboto.ttf")
+pdf.add_font('Roboto-Bold', '', "Roboto-Bold.ttf")
+pdf.add_font('Roboto-Italic', '', "Roboto-Italic.ttf")
+
+pdf.set_font("Roboto-Bold", "", 14)
+margins = 15
+puzzle_margin = 40
+offset = 135
+divider_height = 5
 pdf.set_margins(margins, margins)
 
-column_width = 80
+column_width = 90
+nbr_per_page = args.ppp
+nbr_lines_list_first_page = 45
+nbr_lines_list = 50
 
-def puzzle_page(title, author, rules, image):
-    pdf.add_page()
-    pdf.image(image, margins, puzzle_margin, column_width)
-    pdf.set_font("Helvetica", "B", 20)
-    pdf.write(text=f'{title}\n')
-    pdf.set_font("Helvetica", "I", 20)
-    pdf.write(text=author)
-    pdf.set_font("Helvetica", "", 14)
-    pdf.set_y(puzzle_margin)
+def title_page(title, details):
+    pdf.set_font("Roboto-Bold", "", 30)
+    for elem in title.split('\\n'):
+        pdf.cell(0, h=10, text=elem, new_x=XPos.LMARGIN, new_y=YPos.NEXT, align="C")
+        pdf.set_font("Roboto", "", 20)
+    pdf.cell(0, h=divider_height, new_x=XPos.LMARGIN, new_y=YPos.NEXT)
+    pdf.cell(0, h=divider_height, new_x=XPos.LMARGIN, new_y=YPos.NEXT, border="T")
+    for i in range(len(details)):
+        pdf.set_font("Roboto", "", 14)
+        pdf.cell(0, h=5, text=f'{i+1} - {details[i][1]} {details[i][2]}\n', new_x=XPos.LMARGIN, new_y=YPos.NEXT, link=details[i][5])
+
+def puzzle_page(nbr, link, title, author, rules, image):
+    position = nbr%nbr_per_page
+    pdf.set_y(position*(offset-divider_height)+margins)
+    if position != 0:
+        pdf.cell(0, h=divider_height, new_x=XPos.LMARGIN, new_y=YPos.NEXT, border="T")
+    pdf.image(image, margins, position*offset+puzzle_margin, column_width, link=link)
+    pdf.set_font("Roboto-Bold", "", 20)
+    anchor = pdf.add_link()
+    pdf.set_link(anchor, position*offset+margins)
+    pdf.write(text=f'{nbr+1} - {title}\n')
+    pdf.set_font("Roboto-Italic", "", 20)
+    pdf.write(text=f'{author}\n')
+    pdf.set_font("Roboto", "", 14)
+    pdf.write(text=link, link=link)
+    pdf.set_font("Roboto", "", 12)
+    pdf.set_y(position*offset+puzzle_margin)
     pdf.set_x(-(margins + column_width))
     pdf.multi_cell(column_width, None, rules)
+    return anchor
+
 
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
@@ -72,15 +101,34 @@ def get_image_and_rules(url):
     driver.close()
     return title, author, rules, img
 
-lines = args.input.readlines()
+lines = args.input.read().splitlines()
+pack_title = ''
+details = []
 for line in lines:
     print(line)
     if line == '':
         continue
     if not bool(url_check.match(line)):
-        print('fail')
+        pack_title = line
         continue
-    puzzle_page(*get_image_and_rules(line))
+    title, author, rules, img = get_image_and_rules(line)
+    
+    details.append([line, title, author, rules, img])
+
+# Allow more pages for title page if it will overflow
+if len(details) > nbr_lines_list_first_page:
+    pdf.add_page()
+for i in range((len(details)-nbr_lines_list_first_page)//nbr_lines_list):
+    pdf.add_page()
+
+for i in range(len(details)):
+    if i%nbr_per_page == 0:
+        pdf.add_page()
+    details[i].append(puzzle_page(i, *details[i]))
+
+pdf.page = 1
+pdf.set_y(margins)
+title_page(pack_title, details)
 
 filename = ''
 if args.output == '':
