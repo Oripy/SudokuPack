d1c70a6183b3a5f25ce34f0076947a3712218db1
Pierre Maurier <pierre.maurier@gadz.org>
2025-04-17 12:00:29 +0200

    fix: bad implementation

--- sudokupack.py
+++ sudokupack.py
@@ -83,16 +83,16 @@ for line in lines:
         url, author_given = line.split()[:2]
         rules_given = " ".join(line.split()[2:])
     else:
-        url, author_given = line, ""
-    if not bool(url_check.match(line)):
+        url, author_given, rules_given = line, "", ""
+    if not bool(url_check.match(url)):
         pack_title = line
         continue
-    real_url, title, author, rules, img, source = get_image_and_rules(line)
+    real_url, title, author, rules, img, source = get_image_and_rules(url)
     if author == "":
         author = author_given
     if rules == "":
         rules = rules_given
-    details.append([line, real_url, title, author, rules, img, source])
+    details.append([url, real_url, title, author, rules, img, source])
 
 # Allow more pages for title page if it will overflow
 if len(details) > nbr_lines_list_first_page:
