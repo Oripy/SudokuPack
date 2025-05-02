925b80f91f4c822b4853839ae6e73fa712132e6e
Pierre Maurier <pierre.maurier@gadz.org>
2025-05-02 13:20:13 +0200

    Adding a way to add text intro and add alternative urls to puzzles

--- sudokupack.py
+++ sudokupack.py
@@ -35,24 +35,27 @@ column_width = 90
 nbr_lines_list_first_page = 45
 nbr_lines_list = 50
 
-def title_page(title, details):
+def title_page(title, intro, details):
     pdf.set_font("Roboto-Bold", "", 30)
     for elem in title.split('\\n'):
         pdf.cell(0, h=10, text=elem, new_x=XPos.LMARGIN, new_y=YPos.NEXT, align="C")
         pdf.set_font("Roboto", "", 20)
     pdf.cell(0, h=divider_height, new_x=XPos.LMARGIN, new_y=YPos.NEXT)
     pdf.cell(0, h=divider_height, new_x=XPos.LMARGIN, new_y=YPos.NEXT, border="T")
+    pdf.set_font("Roboto", "", 14)
+    if intro != '':
+        pdf.cell(0, h=5, text=f'{intro}', new_x=XPos.LMARGIN, new_y=YPos.NEXT)
+        pdf.cell(0, h=divider_height, new_x=XPos.LMARGIN, new_y=YPos.NEXT, border="T")
     for i in range(len(details)):
-        pdf.set_font("Roboto", "", 14)
         pdf.cell(0, h=5, text=f'{i+1} - {details[i][2]}{f' by {details[i][3]}' if details[i][3] != "" else ""}\n', new_x=XPos.LMARGIN, new_y=YPos.NEXT, link=details[i][7])
 
-def puzzle_page(nbr, link, real_url, title, author, rules, image, source=""):
+def puzzle_page(nbr, links, real_url, title, author, rules, image, source=""):
     global skipped
     position = (nbr + skipped)%nbr_per_page
     pdf.set_y(position*(offset-divider_height)+margins)
     if position != 0:
         pdf.cell(0, h=divider_height, new_x=XPos.LMARGIN, new_y=YPos.NEXT, border="T")
-    pdf.image(image, margins, position*offset+puzzle_margin, column_width, link=link)
+    pdf.image(image, margins, position*offset+puzzle_margin, column_width, link=real_url)
     pdf.set_font("Roboto-Bold", "", 20)
     anchor = pdf.add_link()
     pdf.set_link(anchor, position*offset+margins)
@@ -61,7 +64,10 @@ def puzzle_page(nbr, link, real_url, title, author, rules, image, source=""):
         pdf.set_font("Roboto-Italic", "", 20)
         pdf.write(text=f'by {author}\n')
     pdf.set_font("Roboto", "", 14)
-    pdf.write(text=link, link=real_url)
+    pdf.write(text=links[0], link=real_url)
+    pdf.set_font("Roboto", "", 12)
+    for link in links[1:]:
+        pdf.write(text=f'Alt link: {link}', link=link)
     pdf.set_font("Roboto", "", 12)
     pdf.set_y(position*offset+puzzle_margin)
     pdf.set_x(-(margins + column_width))
@@ -81,25 +87,37 @@ from puzzle_url_tools import get_image_and_rules
 
 lines = args.input.read().splitlines()
 pack_title = ''
+pack_intro = ''
 details = []
 for line in lines:
     if line == '':
         continue
     print(line)
-    if len(line.split()) > 1:
-        url, author_given = line.split()[:2]
-        rules_given = " ".join(line.split()[2:])
-    else:
-        url, author_given, rules_given = line, "", ""
-    if not bool(url_check.match(url)):
-        pack_title = line
+    urls = []
+    author_given = ""
+    rules_given = ""
+    splited_line = line.split()
+    for i in range(len(splited_line)):
+        if url_check.match(splited_line[i]):
+            urls.append(splited_line[i])
+        else:
+            author_given = splited_line[i]
+            break
+    if len(splited_line) > (len(urls) + (1 if author_given != "" else 0)):
+        rules_given = " ".join(line.split()[(len(urls) + (1 if author_given != "" else 0)):])
+
+    if len(urls) == 0:
+        if pack_title == '':
+            pack_title = line
+        else:
+            pack_intro += line + '\n'
         continue
-    real_url, title, author, rules, img, source = get_image_and_rules(url)
+    real_url, title, author, rules, img, source = get_image_and_rules(urls[0])
     if author == "":
         author = author_given
     if rules == "":
         rules = rules_given
-    details.append([url, real_url, title, author, rules, img, source])
+    details.append([urls, real_url, title, author, rules, img, source])
 
 # Allow more pages for title page if it will overflow
 if len(details) > nbr_lines_list_first_page:
@@ -114,7 +132,7 @@ for i in range(len(details)):
 
 pdf.page = 1
 pdf.set_y(margins)
-title_page(pack_title, details)
+title_page(pack_title, pack_intro, details)
 
 filename = ''
 if args.output == '':
